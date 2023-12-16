15f76bce4f6b9aa421148617970a73656a5d566c
Henriqueee <henriqueft04@ua.pt>
2023-12-16 00:51:24 +0000

    final

--- main.py
+++ main.py
@@ -22,8 +22,8 @@ T -> Terminar
         choice = str(input("Escolha uma opção: ")).upper()
         if choice == 'A':
             print("Carregar ficheiro selecionado!")
-            #filename = str(input("Introduza o nome do ficheiro: "))
-            dados = carregar_ficheiro('./geoq.csv')      
+            filename = str(input("Introduza o nome do ficheiro: "))
+            dados = carregar_ficheiro(filename)      
         elif choice == 'B':
             composto = str(input("Introduza o nome do composto: "))
 
@@ -60,9 +60,14 @@ T -> Terminar
                     n_classes = int(input("Introduza o número de classes: "))
                     histograma(dados, composto_x, n_classes) 
         elif choice == 'E':
-            print("Visualizar mapa dos tipos de granito")
+            print("Visualizar mapa dos tipos de granito selecionado!")
+            dispersao_granito(dados)
         elif choice == 'F':
-            print("Visualizar percurso de amostragem efetuado")
+            print("Visualizar percurso de amostragem efetuado selecionado!")
+            percurso_amostragem(dados)
+        elif choice == 'H':
+            print("Visualizar dados repetidos selecionado!")
+            verificar_linhas_repetidas(filename)
         elif choice == 'T':
             print("Terminar")
             break
@@ -84,15 +89,18 @@ def carregar_ficheiro(filename):
                 coordenadas = tuple(linha[:2])  # Os primeiros dois valores são as coordenadas
                 valores = linha[2:]  # O resto dos valores são os valores dos químicos
                 valores = [float(valor) for valor in valores]
-                valores_filtrados = [x for x in valores if x != -9999]
+                valores_filtrados = [x for x in valores if x != -99999.0]
                 dados[coordenadas] = valores_filtrados
 
     except FileNotFoundError:
         print('Ficheiro não encontrado!')
+        
+        return None
+
     except Exception as e:
         print('Um erro ocorreu!:', e)
 
-    print("Ficheiro '%s' carregado com sucesso!" % filename)
+    print("Ficheiro '%s' carregado com sucesso!\n" % filename)
     return dados
 
 def estatisticas_basicas(dados, composto):
@@ -105,17 +113,18 @@ def estatisticas_basicas(dados, composto):
 
     media = sum([x[compostos_indices[composto]] for x in dados.values()]) / len(dados)
 
+    print('------------------------------------------------')
     print('Valor mínimo de %s = %.4f' % (composto, minimo))
     print('Valor máximo de %s = %.4f' % (composto, maximo))
     print('Valor médio de %s = %.4f' % (composto, media))
-
+    print('------------------------------------------------')
 
 def grafico_dispersao(dados, composto_x, composto_y):
 
     x = [x[compostos_indices[composto_x]] for x in dados.values()]
     y = [x[compostos_indices[composto_y]] for x in dados.values()]
 
-    plt.scatter(x, y)
+    plt.scatter(x, y, s=7) # parametro s torna pontos mais finos
     plt.xlabel(composto_x + '(%)')
     plt.ylabel(composto_y + '(%)')
     plt.title('Gráfico de dispersão')
@@ -131,13 +140,106 @@ def histograma(dados, composto, n_classes):
     plt.title('Histograma do composto %s (%%) (%d classes)' % (composto, n_classes))
     plt.show()
 
+def dispersao_granito(dados):
+
+    coord_x_tipo1 = []
+    coord_y_tipo1 = []
+    coord_x_tipo2 = []
+    coord_y_tipo2 = []
+
+    for key, value in dados.items():
+        print(key, value)
+        if len(value) == 4:
+            if value[3] < 2:
+                coord_x_tipo1.append(float(key[0]))
+                coord_y_tipo1.append(float(key[1]))
+            elif value[3] >= 2:
+                coord_x_tipo2.append(float(key[0]))
+                coord_y_tipo2.append(float(key[1]))
+
+    # Criar o gráfico de dispersão
+    plt.scatter(coord_x_tipo1, coord_y_tipo1, label='Granito de tipo I', color='blue')
+    plt.scatter(coord_x_tipo2, coord_y_tipo2, label='Granito de tipo II', color='red')
+
+    # Configurar labels e título do gráfico
+    plt.xlabel('Coordenada X')
+    plt.ylabel('Coordenada Y')
+    plt.title('Mapa de amostragem')
+    plt.legend()
+    plt.show()
+    
+def percurso_amostragem(dados):
+    
+    # Se as coordenadas forem strings, converta-as em números
+    coordenadas = [(float(x), float(y)) for x, y in dados.keys()]
+
+    distancia_total = 0
+    for i in range(1, len(coordenadas)):
+        x1, y1 = coordenadas[i - 1]
+        x2, y2 = coordenadas[i]
+        distancia_total += ((x2 - x1) ** 2 + (y2 - y1) ** 2) ** 0.5
+
+    # Plotar o percurso
+    x, y = zip(*coordenadas)  # Separar as coordenadas em listas x e y
+    plt.scatter(x, y) # Pontos
+    plt.plot(x, y, linestyle='dashed')  # Linha que conecta os pontos
+
+    # Adicionar marcador para o ponto inicial (primeiro elemento)
+    plt.scatter(coordenadas[0][0],coordenadas[0][1] , label='Ponto Inicial',color='r', marker='<',zorder=3)
+
+    # Adicionar marcador para o ponto final (último elemento)
+    plt.scatter(coordenadas[-1][0],coordenadas[-1][1], label='Ponto Final', color='r', marker='s',zorder=3)
+
+    # Configurar rótulos e título do gráfico
+    plt.xlabel('X (Km)')
+    plt.ylabel('Y (Km)')
+    plt.title('Percurso de amostragem (distância percorrida: %.1f Km)' % distancia_total)
+    plt.legend()
+
+    # Exibir o gráfico
+    plt.show()
+
+
+def verificar_linhas_repetidas(filename):
+    
+    try:
+        with open(filename, 'r', newline='', encoding='utf-8') as arquivo:
+            linhas = arquivo.readlines()
+            
+            # Encontrar linhas duplicadas
+
+            linhas_vistas = set()
+            linhas_unicas = []
+            linhas_duplicadas = []
+
+            for linha in linhas:
+                if linha in linhas_vistas:
+                    linhas_duplicadas.append(linha)
+                else:
+                    linhas_vistas.add(linha)
+                    linhas_unicas.append(linha)
+            
+            if not linhas_duplicadas:
+                print(f"O arquivo '{filename}' não possui linhas iguais.")
+            else:
+                print(f"Linhas duplicadas encontradas no arquivo '{filename}':")
+                for linha in linhas_duplicadas:
+                    print(linha.strip())
+                
+                # Perguntar ao utilizador se deseja remover as linhas duplicadas
+                resposta = input("Deseja remover as linhas duplicadas? (S/N): ").strip().lower()
+                if resposta == 's':
+                    # Remover as linhas duplicadas
+                    with open(filename, 'w', newline='', encoding='utf-8') as novo_arquivo:
+                        novo_arquivo.writelines(linhas_unicas)
+                    print("Linhas duplicadas removidas com sucesso.")
+                else:
+                    print("Linhas duplicadas não foram removidas.")
+
+    except FileNotFoundError:
+        print(f"O arquivo '{filename}' não foi encontrado.")
+    except Exception as e:
+        print(f"Ocorreu um erro: {str(e)}")
+
 
 main()
-def histograma(dados, composto, n_classes):
-    valores = [x[compostos_indices[composto]] for x in dados.values()]
-
-    plt.hist(valores, bins=n_classes)
-    plt.xlabel(composto + ' (%)')
-    plt.ylabel('Frequência absoluta')
-    plt.title(f'Histograma do composto {composto} (%) ({n_classes} classes)')
-    plt.show()
